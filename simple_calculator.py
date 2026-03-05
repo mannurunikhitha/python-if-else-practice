3fffa236d022d5815e45b12902ba3da62d3122e5
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-03-06 00:41:04 +0530

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,3 +1,4 @@
+# Simple calculator program
 num1,num2=int(input('Enter Number: ')),int(input('Enter Number: '))
 operator=input("Enter operator (+, -, *, /): ")
 if operator=='+':
@@ -9,4 +10,5 @@ elif operator=='*':
 elif operator=='/':
     print(num1/num2)
 else:
+
     print('Invalid Operator')
