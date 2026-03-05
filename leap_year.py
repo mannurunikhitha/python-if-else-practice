ad707c58290dadb32b74296ce5bf19e554ef4a65
Mannuru Nikhitha <mannurunikhitha@gmail.com>
2026-03-06 00:40:25 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,5 +1,7 @@
+# Program to check whether a given year is a leap year
 year=int(input('Enter year: '))
 if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
     print(f'{year} is a leap year')
 else:
+
     print(f'{year} is not a leap year')
